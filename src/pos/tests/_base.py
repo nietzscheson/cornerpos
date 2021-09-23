4906965fc19a03007fb218a7bbe2b8a9acebbe65
Cristian Angulo Nova <cristianangulonova@gmail.com>
2021-09-23 12:01:25 -0500

    feat: Add login templates and TestCases

--- src/pos/tests/_base.py
+++ src/pos/tests/_base.py
@@ -1,5 +1,4 @@
 import socket
-from urllib.parse import urlparse
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings, tag
@@ -32,25 +31,3 @@ class BaseTestCase(StaticLiveServerTestCase):
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
-
-
-class AdminTest(BaseTestCase):
-    fixtures = ["users"]
-
-    def test_login(self):
-        """
-        As a superuser with valid credentials, I should gain
-        access to the Django admin.
-        """
-        self.selenium.get(self.live_server_url)
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys("george")
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys("password1234")
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
-
-        path = urlparse(self.selenium.current_url).path
-        self.assertEqual("/", path)
-
-        body_text = self.selenium.find_element_by_tag_name("body").text
-        self.assertIn("WELCOME, GEORGE.", body_text)
---
+++ src/pos/tests/tests_admin.py
@@ -0,0 +1,26 @@
+from urllib.parse import urlparse
+from django.urls import reverse
+
+from ._base import BaseTestCase
+
+
+class AdminTest(BaseTestCase):
+    fixtures = ["users"]
+
+    def test_login(self):
+        """
+        As a superuser with valid credentials, I should gain
+        access to the Django admin.
+        """
+        self.selenium.get("%s%s" % (self.live_server_url, reverse("admin:index")))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys("ana")
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys("password1234")
+        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+
+        path = urlparse(self.selenium.current_url).path
+        self.assertEqual(reverse("admin:index"), path)
+
+        body_text = self.selenium.find_element_by_tag_name("body").text
+        self.assertIn("WELCOME, ANA.", body_text)
