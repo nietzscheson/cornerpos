01b8d7be412edd9d7b754a4b5c4f14b41cc28d8a
Cristian Angulo Nova <cristianangulonova@gmail.com>
2022-11-12 11:54:03 -0500

    feat: Adding Factories

---
+++ src/pos/factories.py
@@ -0,0 +1,20 @@
+import factory
+from django.contrib.auth import get_user_model
+
+from faker import Faker
+
+fake = Faker()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    _PASSWORD = "password123!"
+    _COMMON_PASSWORD = "password123"
+
+    password = factory.PostGenerationMethodCall("set_password", _PASSWORD)
+    email = factory.Faker("email")
+    username = factory.Faker("name")
+    is_active = False
+    is_staff = False
+    is_superuser = False
--- src/pos/tests/test_admin.py
+++ src/pos/tests/test_admin.py
@@ -1,17 +1,17 @@
 from urllib.parse import urlparse
 from django.urls import reverse
 from selenium.webdriver.common.by import By
-from pos.tests._base import BaseTestCase
-import pytest
+from pos.factories import UserFactory
+import time
 
 
-# @pytest.mark.usefixtures("users")
 def test_login(selenium, live_server, loaddata):
     """
     As a superuser with valid credentials, I should gain
     access to the Django admin.
     """
-    loaddata({"users"})
+    user = UserFactory(username="ana", first_name="Ana", password="password1234", is_staff=True, is_active=True)
+
     selenium.get("%s%s" % (live_server.url, reverse("admin:index")))
     username_input = selenium.find_element(By.NAME, "username")
     username_input.send_keys("ana")
@@ -19,7 +19,9 @@ def test_login(selenium, live_server, loaddata):
     password_input.send_keys("password1234")
     selenium.find_element(By.XPATH, '//input[@value="Log in"]').click()
 
+
     path = urlparse(selenium.current_url).path
+
     assert reverse("admin:index") == path
 
     body_text = selenium.find_element(By.TAG_NAME, "body").text
